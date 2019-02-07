c2184cee3044acaa5d149b69ea6f1cdc6e594f01
blackwell <blackwellsmithiv@gmail.com>
2019-02-07 16:47:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,10 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
   def add_item(title, price, quantity = 1)
+    @items << title
     previous_total = @total
     @total = @total + (price*(quantity))
   end
