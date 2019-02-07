31f637b831ff8150c744aea127547c7eb3ed1e6d
blackwell <blackwellsmithiv@gmail.com>
2019-02-07 14:00:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
-
+class CashRegister
+  attr_accessor :cash_register, :cash_register_with_discount
+  def initialize
+     @total = 0
+  end
+  def total() #returns correct total
+    
+    
+  end  
+end
