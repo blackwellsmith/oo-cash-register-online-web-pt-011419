b3e0b19d800d2c4efbee319deb87e88b262269b7
blackwell <blackwellsmithiv@gmail.com>
2019-02-07 14:24:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :cash_register, :cash_register_with_discount
+  attr_accessor :cash_register, :cash_register_with_discount, :discount
   def initialize(cash_register_with_discount = cash_register)
      @total = 0
   end
